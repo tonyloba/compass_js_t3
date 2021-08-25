d60b04fa78b7a6bd4ee330c5e9f123cdac99c473
tonyloba <tonyloba@gmail.com>
2021-08-25 22:40:49 +0000

    commited all files from javascripting

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math(width,length,heigth){
+ return length * heigth + width ; 
+}
+console.log(math(53,61,67));
--- introduction.js
+++ introduction.js
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-console.log('Hello AL');
-
-//add simple comment
-=======
-console.log('Hello AL');
->>>>>>> 325a0072b709e4aadaabfc858ea6945354a0d06b
-
-
-// New comment after fixed merging
-//One more comment
-
-// 3rd comment 
+console.log('hello');
